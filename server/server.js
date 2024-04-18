c4c43481f80d6fe9003baf5cc2defac29ee76422
Emmanuel Atuahene <emmanuelatuahene16@gmail.com>
2024-04-18 09:59:55 -0400

    connected to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
-const express =require ("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const { dot } = require('node:test/reporters');
+dotenv.config();
+mongoose.connect(
+  process.env.MONGO_URI).then(() => {
+    console.log('Connected to Database');
+  }).catch((err) => {
+    console.log(err);
+  })
+
+
 const app = express();
 
 app.listen(3000, () => {
