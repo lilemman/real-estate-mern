c4c86b3009627fbca7777553669e64569d2616fa
Emmanuel Atuahene <emmanuelatuahene16@gmail.com>
2024-04-18 12:09:24 -0400

    created auth api route

---
+++ server/controllers/userController.js
@@ -0,0 +1,5 @@
+ const test=(req, res) => {
+ req.body 
+}
+
+module.exports=test;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const { dot } = require('node:test/reporters');
+const userRouter = require('./routes/userRoutes');
+const authRouter = require('./routes/authRoute');
 dotenv.config();
-mongoose.connect(
-  process.env.MONGO_URI).then(() => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log('Connected to Database');
-  }).catch((err) => {
-    console.log(err);
   })
-
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+app.use(express.json())
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
