51bb6897c6646bf7533834bf291e3f1cad265df6
Emmanuel Atuahene <emmanuelatuahene16@gmail.com>
2024-04-17 22:40:43 -0400

    added pages  with routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
+/* eslint-disable no-unreachable */
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn/SignIn';
+import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
+import About from './pages/About/About';
+import Profile from './pages/Profile/Profile';
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>;
+};
 
-export default App
+export default App;
