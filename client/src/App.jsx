bbcf72330fca624864af85b4840935f89ad73268
Emmanuel Atuahene <emmanuelatuahene16@gmail.com>
2024-04-17 23:19:35 -0400

    Header created component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
-/* eslint-disable no-unreachable */
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn/SignIn';
 import Home from './pages/Home/Home';
 import SignUp from './pages/SignUp/SignUp';
 import About from './pages/About/About';
 import Profile from './pages/Profile/Profile';
+import Header from './components/Header/Header';
+
 const App = () => {
-  return;
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,7 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div className="text-3xl font-bold underline">Home</div>;
 };
 
 export default Home;
